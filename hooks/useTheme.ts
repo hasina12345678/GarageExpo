88997040bc9b1cf707f7244693ff597832792cba
Hasina <andrianirinahasina07.com>
2026-02-10 17:25:11 +0300

    V9

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,23 +1,16 @@
-// hooks/useTheme.ts
-import { useState, useEffect } from 'react';
-import themeService from '../services/Theme/ThemeService';
+
+import themeService, { ThemeType } from '../services/Theme/ThemeService';
+
+export function getCurrentTheme() {
+  return {
+    theme: themeService.getCurrentTheme(),
+    colors: themeService.getColors()
+  };
+}
 
 export function useTheme() {
-  const [theme, setTheme] = useState(themeService.getCurrentTheme());
-  const [colors, setColors] = useState(themeService.getColors());
-
-  useEffect(() => {
-    const handleThemeChange = (newTheme: any) => {
-      setTheme(newTheme.detail);
-      setColors(themeService.getColors());
-    };
-
-    window.addEventListener('themeChanged', handleThemeChange);
-
-    return () => {
-      window.removeEventListener('themeChanged', handleThemeChange);
-    };
-  }, []);
-
+  const theme = themeService.getCurrentTheme();
+  const colors = themeService.getColors();
+  
   return { theme, colors };
 }
